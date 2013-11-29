e29ecf0507be12f81b545f9230e06cc579bad390
Steven Moseley <smoseley@transio.com>
2013-11-29 10:42:15 -0500

    Removed old config files

--- app/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config = new \Qi\Config();
-
-$config->write("path.base", dirname(dirname(__FILE__)));
-$config->write("path.controllers", dirname(dirname(__FILE__)));
-$config->write("database.dsn", "sss");
-$config->write("database.username", "root");
-$config->write("database.password", "");
--- app/local.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$config->write("database.dsn", "sss");
-$config->write("database.username", "root");
-$config->write("database.password", "");
